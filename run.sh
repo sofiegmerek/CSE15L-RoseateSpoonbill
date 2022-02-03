6bc4cc935983c43bddbd4f04e97ebe8ae2f6dde8
sofie <sofie.e.gmerek@gmail.com>
2022-02-03 13:10:58 -0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+
+java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
+echo this is first
+echo this is seond
